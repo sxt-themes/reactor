0c1be8c8f0f3a2ab8b5bf3dc1726da45dd337caa
s h a w <75453555+shaww0@users.noreply.github.com>
2022-04-05 04:29:19 +0300

    Add files via upload

---
+++ captures/config.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'secure_key' => 'KEY_HERE',
+
+    'output_url' => 'https://reactor.codemafia.org/captures/',
+
+    'redirect_url' => 'https://reactor.codemafia.org/captures/',
+
+    'allowed_ips' => array(),
+
+    'page_title' => 'page_title',
+
+    'heading_text' => 'heading_text',
+);
